3aed138202e3dca1736a57e72cde484a8999828d
meleo2125 <mukeshprajapat3093@gmail.com>
2025-06-21 00:49:46 +0530

    build #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navigation listings={listings} />
+      <Navigation />
 
       {/* Main Content */}
       <main className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
