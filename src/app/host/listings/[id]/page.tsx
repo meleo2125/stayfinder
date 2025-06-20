7d0bfd5525dfd0c55a8a24ab92411917f6e04b2d
meleo2125 <mukeshprajapat3093@gmail.com>
2025-06-20 19:43:47 +0530

    build #1

--- src/app/host/listings/[id]/page.tsx
+++ src/app/host/listings/[id]/page.tsx
@@ -331,7 +331,7 @@ function HostListingDetailPage() {
   );
 }
 
-export default function HostListingDetailWrapper(props) {
+export default function HostListingDetailWrapper(props: any) {
   return (
     <HostAuthProvider>
       <HostListingDetailPage {...props} />
